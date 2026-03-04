6ae29767a84553eb954085cf4b7a82117605b49d
Wisdom Ogwu <iammadab@gmail.com>
2026-03-04 22:46:53 +0100

    custom test runner with exit device

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@
 // as we are trying to create our own operating system
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -21,11 +24,41 @@ fn panic(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-// VGA Notes
-// there is some buffer for VGA text
-// seems you write 2 bytes, the first
-// represents the ascii character
-// and the second is the color / attribute
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        // this exit port is already registered with qemu
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
