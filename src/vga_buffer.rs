36f1d565a2adeff2187d245e96278abc3d2686c8
Wisdom Ogwu <iammadab@gmail.com>
2026-03-04 20:16:01 +0100

    define color code + screenchar + buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,6 @@
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_HEIGHT: usize = 80;
+
 #[repr(u8)]
 pub enum Color {
     Black,
@@ -25,4 +28,14 @@ impl ColorCode {
     fn new(foreground: Color, background: Color) -> Self {
         Self((background as u8) << 4 | foreground as u8)
     }
+}
+
+/// Represents a character on the screen
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
