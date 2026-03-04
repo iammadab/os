9346f1ed9309a11c245c18a8cad2df62d1373595
Wisdom Ogwu <iammadab@gmail.com>
2026-03-04 19:53:49 +0100

    basic bootable kernel + simple VGA print

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,27 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World, how is it going?";
+
 /// Program entry point
 /// should not return hence !
 /// as this function is not called by some other function
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
+
+// VGA Notes
+// there is some buffer for VGA text
+// seems you write 2 bytes, the first
+// represents the ascii character
+// and the second is the color / attribute
