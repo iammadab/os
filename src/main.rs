e6a39f55c9b9058427cf7986117cf50c6542c7a3
Wisdom Ogwu <iammadab@gmail.com>
2026-03-04 20:33:11 +0100

    using the vga buffer to write to screen

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+use crate::vga_buffer::print_something;
+
 /// this is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -21,14 +23,7 @@ static HELLO: &[u8] = b"Hello World, how is it going?";
 /// as this function is not called by some other function
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    print_something();
 
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -90,3 +90,14 @@ impl Writer {
         }
     }
 }
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello World!");
+}
