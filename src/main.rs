1be065845f3536224116d95ee8076d8ebe5ffdec
Wisdom Ogwu <40731160+iammadab@users.noreply.github.com>
2026-03-04 23:59:12 +0100

    Testing (#3)
    * custom test runner with exit device
    
    * refactor test to use serial print
    
    * separate panic handler for test and not test
    
    * remove display on test
    
    * better testing framework
    
    * clean up + add should panic harness

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 // the goal here is to create a freestanding binary
 // that does not depend on any of the os features
 // as we are trying to create our own operating system
-#![no_std]
-#![no_main]
 
-mod vga_buffer;
+#![no_main]
+#![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-/// this is called on panic
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
+use os::println;
 
 /// Program entry point
 /// should not return hence !
@@ -21,11 +17,23 @@ fn panic(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-// VGA Notes
-// there is some buffer for VGA text
-// seems you write 2 bytes, the first
-// represents the ascii character
-// and the second is the color / attribute
+// panic handler
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
+}
