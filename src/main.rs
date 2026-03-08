6e37c0c99ae18ff5bad291eda69173e922eb5cf4
Wisdom Ogwu <iammadab@gmail.com>
2026-03-08 22:30:46 +0100

    remove create mapping example

--- src/main.rs
+++ src/main.rs
@@ -18,28 +18,10 @@ entry_point!(kernel_main);
 /// should not return hence !
 /// as this function is not called by some other function
 #[unsafe(no_mangle)]
-fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use os::memory;
-    use os::memory::BootInfoFrameAllocator;
-    use x86_64::{structures::paging::Page, VirtAddr};
-
+fn kernel_main(_boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     os::init();
-
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // write the string `New!` to the screen through the mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe {
-        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f046);
-    }
 
     #[cfg(test)]
     test_main();
